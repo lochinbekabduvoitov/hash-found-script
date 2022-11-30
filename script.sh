f22eb73e253a5954330327535360bc04cd4dcc88
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2022-11-30 21:45:59 +0500

    Compairson and than if conditional statement

--- script.sh
+++ script.sh
@@ -7,7 +7,6 @@
 #VARAIBLES
 #Uppercase by caonvention
 
-NAME="hack"
 
 # echo "My name is $NAME"
 # echo "My name is ${NAME}"
@@ -34,10 +33,42 @@ NAME="hack"
 #     echo "Your name is not Lochinbek"
 # fi
 
+# if [ "$NAME" == "Lochinbek" ]
+# then 
+#     echo "Your name is Lochinbek"
+# else
+#     echo "Your name is not Lochinbek"
+# fi
 
-if [ "$NAME" == "Lochinbek" ]
-then 
-    echo "Your name is Lochinbek"
-else
-    echo "Your name is not Lochinbek"
-fi
+# NAME="Le"
+#ELSE-IF (elseif)
+
+# if [ "$NAME" == "Lochinbek" ]
+# then 
+#     echo "Your name is Lochinbek"
+# elif [ "$NAME" == "Jack" ]
+# then
+#     echo "Your name is  Jack"
+#     else
+#     echo "Your nane is NOT Lochinbek or Jack"
+# fi
+
+#COMPARISON
+
+# There are other comparison operators for numbers listed below:
+# -ne - not equal
+# -lt - less than
+# -gt - greater than
+# -le - less than or equal to
+# -ge - greater than or equal to
+
+# NUM1=3
+# NUM2=5
+
+# if ["$NUM1" -gt "$NUM2"]
+# then
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less then $NUM2"
+# fi
+
