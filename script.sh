14eeefe5012983cbf102693de37c66d595b83531
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2022-11-30 21:32:47 +0500

    Start script shell

---
+++ script.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/bash
+
+#ECHO COMMMAND
+
+# echo Hello world
+
+#VARAIBLES
+#Uppercase by caonvention
+
+NAME="hack"
+
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+# USER input 
+
+# read -p "Enter your name: " NAME
+
+# echo "Hello $NAME , nice to meet you!"
+
+#SIMPLE STATEMENT
+
+# if [ "$NAME" == "Lochinbek" ]
+# then 
+#     echo "Your name is Lochinbek"
+# fi
+
+
+#IF ELSE
+# if [ "$NAME" == "Lochinbek" ]
+# then 
+#     echo "Your name is Lochinbek"
+# else
+#     echo "Your name is not Lochinbek"
+# fi
+
+
+if [ "$NAME" == "Lochinbek" ]
+then 
+    echo "Your name is Lochinbek"
+else
+    echo "Your name is not Lochinbek"
+fi
