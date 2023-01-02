d09743d9ed64a2cda7669ec4e9a1b2d5a1bdd434
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-01-02 11:40:23 +0500

    hash

--- find-v2.sh
+++ find-v2.sh
@@ -1,5 +1,20 @@
 #! /usr/bin/bash
 
 read -p "Enter file directory/" NAME
+read -p "Enter file name/" HASH
 
-find /home/$NAME -name *.txt | grep -v .vscode
+RESULT=`md5sum $HASH`
+
+find /home/$NAME -name *$RESULT.jpg   FILES
+
+# for FILE in $FILES
+#     do
+#         HASHING=`md5sum $FILE`
+#         if [ "$RESULT" == "$HASHING" ]
+#         then 
+#             echo "Succes $HASHING  file is founded"
+#         else
+#             echo "Not found"
+#         fi
+# done
+
